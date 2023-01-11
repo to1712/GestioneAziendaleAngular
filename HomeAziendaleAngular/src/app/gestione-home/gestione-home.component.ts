b28ce12eaf573a1d18c16635e31ca3b8727d064f
Fran2909 <pantusa_francesco@libero.it>
2023-01-11 11:16:43 +0100

    switch

--- HomeAziendaleAngular/src/app/gestione-home/gestione-home.component.ts
+++ HomeAziendaleAngular/src/app/gestione-home/gestione-home.component.ts
@@ -8,17 +8,13 @@ import { ServiceService } from '../service/service.service';
   styleUrls: ['./gestione-home.component.css'],
 
 })
-export class GestioneHomeComponent implements OnInit{
-    it=false;
-    mns=false;
+export class GestioneHomeComponent implements OnInit {
+    res:string|null = "";
 
     ngOnInit(): void {
       const urlParams = new URLSearchParams(window.location.search);
       var sessionId = urlParams.get("jsessionid");
-      if(sessionId === 'it')
-        this.it = true;
-      else if(sessionId === 'mns')
-      this.mns = true;
+      this.res = sessionId;
     }
 
 }
