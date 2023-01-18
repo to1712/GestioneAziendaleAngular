fe4bb5786e5a15d39cecacd13553ebb982da5aac
Fran2909 <pantusa_francesco@libero.it>
2023-01-18 21:02:49 +0100

    alcune modifiche grafiche

--- HomeAziendaleAngular/src/assets/js/scripts.js
+++ HomeAziendaleAngular/src/assets/js/scripts.js
@@ -3,9 +3,9 @@
     * Copyright 2013-2022 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
-    // 
+    //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -26,3 +26,48 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+
+/*window.addEventListener('DOMContentLoaded', event => {
+
+  // Toggle the side navigation
+  const navlink = document.body.querySelector('nav-link collapsed');
+  if (navlink) {
+      // Uncomment Below to persist sidebar toggle between refreshes
+      // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+      //     document.body.classList.toggle('sb-sidenav-toggled');
+      // }
+      navlink.addEventListener('click', event => {
+          event.preventDefault();
+          //document.body.classList.toggle('sb-sidenav-toggled');
+
+          document.getElementById("open").classList.toggle("collapsed");
+          document.getElementById("open").classList.remove("collapsed");
+          //localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+          localStorage.setItem('collapse', document.getElementById("collapseLayouts").classList.contains('collapse'));
+      });
+  }
+
+});*/
+
+window.addEventListener('DOMContentLoaded', event => {
+  let isExpanded = false;
+  document.getElementById("open").addEventListener("click", event => {
+  if(isExpanded){
+  document.getElementById("open").classList.add("collapsed");
+  document.getElementById("open").setAttribute("aria-expanded", "false");
+  document.getElementById("collapseLayouts").classList.toggle("show");
+  isExpanded = false;
+  }else{
+  document.getElementById("open").classList.remove("collapsed");
+  document.getElementById("open").setAttribute("aria-expanded", "true");
+  document.getElementById("collapseLayouts").classList.toggle("show");
+  isExpanded = true;
+  }
+  });
+  });
+
+
+
+
+
+
