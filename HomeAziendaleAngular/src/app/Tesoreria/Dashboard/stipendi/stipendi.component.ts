a5e96efa7ad79c07fd0a71d4a3b94308fbd67081
to1712 <toto.orrico@gmail.com>
2023-01-19 19:59:04 +0100

    commit

--- HomeAziendaleAngular/src/app/Tesoreria/Dashboard/stipendi/stipendi.component.ts
+++ HomeAziendaleAngular/src/app/Tesoreria/Dashboard/stipendi/stipendi.component.ts
@@ -14,23 +14,21 @@ import {MatTableDataSource} from '@angular/material/table';
 })
 export class StipendiComponent implements OnInit,AfterViewInit {
   utenti:Utente[]=[];
-  displayedColumns: string[] = ['nome', 'cognome', 'stipendio'];
+  displayedColumns: string[] = ['nome', 'cognome','stipendio'];
   dataSource: MatTableDataSource<Utente>=new MatTableDataSource();
   @ViewChild(MatPaginator) paginator!:MatPaginator;
   @ViewChild(MatSort) sort!:MatSort;
+ 
   
   constructor(private s:DatabaseService){
     this.s.getUtenti().subscribe((ut)=>{this.utenti=ut
-      this.dataSource = new MatTableDataSource(this.utenti);
+      this.dataSource = new MatTableDataSource(this.utenti)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
-   // this.dataSource.paginator = this.paginator;
-   // this.dataSource.sort = this.sort;
-  }
 
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
   }
+  ngAfterViewInit(): void {}
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
